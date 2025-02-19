df53af80308b051e8473278402b4a1ef0108ba11
Kangwook Lee <43847427+kvngwxxk@users.noreply.github.com>
2025-02-19 16:51:59 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/kvngwxxk/AlignedCollectionViewFlowLayout.git", branch: "master")
+        .package(url: "https://github.com/kvngwxxk/AlignedCollectionViewFlowLayout.git", from: "1.0.0")
 
     ],
     targets: [
