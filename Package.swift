fb4dfc91f27b6d259278c4f9d592eb015baeab48
Kangwook Lee <43847427+kvngwxxk@users.noreply.github.com>
2025-02-19 16:24:49 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,9 @@ let package = Package(
                 .product(name: "AlignedCollectionViewFlowLayout", package: "AlignedCollectionViewFlowLayout")
             ],
             path: "Hashtags" // 소스 디렉토리 경로
+            resources: [
+                .process("Assets") // Assets 폴더 내의 리소스를 번들에 포함시킴
+            ]
         )
     ]
 )
